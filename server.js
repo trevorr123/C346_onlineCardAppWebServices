ee13ce53094f7a529651cbf48ecbe189a479bd2b
trevorr123 <24031694@myrp.edu.sg>
2026-01-07 12:01:07 +0800

    commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+const port = 3000;
+
+//database config info
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0,
+};
+
+//initialize Express app
+const app = express();
+
+//helps app read JSON
+app.use(express.json());
+
+//start server
+app.listen(port, () => {
+    console.log('Server running on port', port);
+});
+
+// Example Route: Get all cards
+app.get('/allcards', async (req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server Error for allcards' });
+    }
+});
